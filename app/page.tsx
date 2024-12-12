611ab2b8f63979373d9ab48fd4b7b81b4f6676aa
KazuyaSuzuki <83203852+ve1997@users.noreply.github.com>
2024-12-12 14:58:49 +0900

    chore: Cloudflareにデプロイするためのセットアップ

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+export const runtime = "edge";
+
 export default function Home() {
 	return (
 		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-	/* config options here */
+const createConfig = async (): Promise<NextConfig> => {
+	if (process.env.NODE_ENV === "development") {
+		await setupDevPlatform();
+	}
+	const nextConfig: NextConfig = {
+		/* config options here */
+	};
+
+	return nextConfig;
 };
 
-export default nextConfig;
+export default createConfig();
