efb871e41de2b99cb242bf61f46718322c7055a6
KazuyaSuzuki <83203852+ve1997@users.noreply.github.com>
2024-12-12 21:05:00 +0900

    feat: Sessionを拡張してgithubのユーザー名を取得・表示

--- auth.ts
+++ auth.ts
@@ -1,6 +1,45 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import "next-auth/jwt";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	providers: [GitHub],
+	callbacks: {
+		async jwt({ token, profile }) {
+			if (profile) {
+				const { login } = profile;
+				token.user = { ...token.user, login };
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			const { login } = token.user;
+			session.user = { ...session.user, login };
+			return session;
+		},
+	},
 });
+
+declare module "next-auth" {
+	interface Session {
+		user: {
+			login: string;
+		} & DefaultSession["user"];
+	}
+
+	interface User {
+		login: string;
+	}
+
+	interface Profile {
+		login: string;
+	}
+}
+
+declare module "next-auth/jwt" {
+	interface JWT {
+		user: {
+			login: string;
+		};
+	}
+}
